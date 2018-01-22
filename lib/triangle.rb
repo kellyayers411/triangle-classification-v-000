b59c8f66cc2fea682bfce5dd009206b04a321fd0
K Logic <kellyayers411@gmail.com>
2018-01-22 20:04:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
   # write code here
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+  @length3 = length3
+  @length2 = length2
+  @length1 = length1
+  end
+
+  def kind 
+  if @length1 == @length2 && @length1 == @length3
+    :equilateral
+  elsif
+    
+  end
+
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end

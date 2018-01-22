be646440bff5761b9033107096603c7834c79bb3
K Logic <kellyayers411@gmail.com>
2018-01-22 20:17:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind 
-  if (@length1 < 0 && @length2 < 0 && @length3 < 0)
+  if (@length1 > 0 && @length2 > 0 && @length3 > 0)
     if @length1 == @length2 && @length1 == @length3
       :equilateral
     elsif ((@length1 == @length2 && @length1 != @length3) || (@length1 == @length3 && @length1 != @length2) || (@length2 == @length3 && @length1 != @length2))
